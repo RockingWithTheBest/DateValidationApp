1f044202083c6939363238f60d4ee65e12ae3e58
RockingWithTheBest <sikanweclyde@gmail.com>
2025-04-24 13:27:06 +0300

    Add project files.

---
+++ DateValidationApp/LibraryDateTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace DateValidationApp
+{
+    public class LibraryDateTests
+    {
+        private static readonly DateTime StartDate = new DateTime(1900, 1, 1);
+        private static readonly DateTime CurrentDate = DateTime.Now;
+
+        public bool ValidDate(string dateInput)
+        {
+            DateTime date;
+            Regex regex = new Regex(@"^\d{2}\.\d{2}\.\d{4}$");
+            if (!regex.IsMatch(dateInput))
+            {
+                date = default;
+                return false;
+            }
+
+
+            if (DateTime.TryParseExact(dateInput, "dd.MM.yyyy",
+                CultureInfo.InvariantCulture, DateTimeStyles.None, out date))
+            {
+                return date >= StartDate && date <= CurrentDate;
+            }
+            date = default;
+            return false;
+        }
+    }
+}
---
+++ TestProject/UnitTest.cs
@@ -0,0 +1,99 @@
+﻿using DateValidationApp;
+using FluentAssertions;
+
+namespace TestProject
+{
+    public class UnitTest
+    {
+        LibraryDateTests libary = new LibraryDateTests();
+
+        //Позитивный тест кайс
+        [Fact]
+        public void LibraryDateTests_ValidDate_LowerBoundary()
+        {
+            var result = libary.ValidDate("01.01.1900");
+            result.Should().BeTrue("the date should be accepted as it is within the valid range.");
+        }
+        [Fact]
+        public void LibraryDateTests_ValidDate_MiddleRange()
+        {
+            var result = libary.ValidDate("01.01.2000");
+            result.Should().BeTrue("the date should be accepted as it is within the valid range.");
+        }
+        [Fact]
+        public void LibraryDateTests_ValidDate_CurrentYear()
+        {
+            var result = libary.ValidDate("31.01.2025");
+            result.Should().BeTrue("the date should be accepted as it is within the valid range.");
+        }
+        [Fact]
+        public void LibraryDateTests_ValidDate_LeapYear()
+        {
+            var result = libary.ValidDate("29.02.2020");
+            result.Should().BeTrue("the date should be accepted as it is a valid leap year date.");
+        }
+
+        //Негативный тест кайс
+        [Fact]
+        public void LibraryDateTests_ValidDate_BelowLowerBoundary()
+        {
+            var result = libary.ValidDate("31.12.1899");
+            result.Should().BeFalse("the date should be rejected as it is below the valid range.");
+        }
+        [Fact]
+        public void LibraryDateTests_ValidDate_AboveUpperBoundary()
+        {
+            var result = libary.ValidDate("01.01.2026");
+            result.Should().BeFalse("the date should be rejected as it is above the valid range.");
+        }
+
+        [Fact]
+        public void Test_ValidDate_AboveCurrentYear()
+        {
+            var result = libary.ValidDate("01.01.2026");
+            result.Should().BeFalse("the date should be rejected as it is above the valid range.");
+        }
+        [Fact]
+        public void Test_ValidDate_MoreThan31Numbers()
+        {
+            var result = libary.ValidDate("32.01.2020");
+            result.Should().BeFalse("the date should be rejected as it is " +
+                "above the valid range as the entered date has more than 31 days.");
+        }
+        [Fact]
+        public void Test_ValidDate_NonAlphabeticCharacters()
+        {
+            var result = libary.ValidDate("#R=%^");
+            result.Should().BeFalse("the date should be rejected as it is " +
+                "above the valid range as the entered has non alphabetic characters.");
+        }
+        [Fact]
+        public void Test_ValidDate_AlphabeticCharacters()
+        {
+            var result = libary.ValidDate("fxggg");
+            result.Should().BeFalse("the date should be rejected as it is " +
+                "above the valid range as the entered has alphabetic characters.");
+        }
+        [Fact]
+        public void Test_ValidDate_MoreThan12Months()
+        {
+            var result = libary.ValidDate("32.67.2020");
+            result.Should().BeFalse("the date should be rejected as it is " +
+                "above the valid range as the entered has more than 12 months.");
+        }
+        [Fact]
+        public void Test_ValidDate_DateHasMoreThanTwoDots()
+        {
+            var result = libary.ValidDate("0.3.11.2020");
+            result.Should().BeFalse("the date should be rejected as it is " +
+                "above the valid range as the entered has more than two DOTS.");
+        }
+        [Fact]
+        public void Test_ValidDate_EmptyDate()
+        {
+            var result = libary.ValidDate("");
+            result.Should().BeFalse("the date should be rejected as it is " +
+                "above the valid range as the entered has more than two DOTS.");
+        }
+    }
+}
